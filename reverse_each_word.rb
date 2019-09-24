62c8f8963744295ee594894731db4fd2a4b29c5a
Buford Eeds <bufordeeds8@gmail.com>
2019-09-24 20:33:26 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -3,7 +3,7 @@ greeting =  "Hello, how are you?"
 def reverse_each_word(sentence)
 # ary = [];
 splitStr = sentence.split(" ")
-reversedSplitStr = splitStr.each { |str| str.reverse  }
+reversedSplitStr = splitStr.collect { |str| str.reverse  }
 reversedStr = reversedSplitStr.join(" ")
 
 end
